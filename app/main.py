198694dff1142d7a16b8b5aa3e2e1fa521ce2072
Vijay Singh <vijaysg@altair.com>
2024-11-15 11:51:09 +0530

    Stage 3

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
-import socket  # noqa: F401
+import socket
+import threading  # noqa: F401
 
-
+def handle_connection(connection):
+    while connection.recv(1024):
+        response = b"+PONG\r\n"
+        connection.send(response)
 def main():
     with socket.create_server(("localhost", 6379), reuse_port=False) as server_socket:
         while True:
-            connecton, _ = server_socket.accept()
-            while connecton.recv(1024):
-                response = b"+PONG\r\n"
-                connecton.send(response)
+            connection, _ = server_socket.accept()
+            client_thread = threading.Thread(target=handle_connection, args=(connection,))
+            client_thread.start()
 
 
 if __name__ == "__main__":
