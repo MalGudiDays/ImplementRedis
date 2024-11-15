90d1e04756a2acf731650201bbfda4016f6260b0
Vijay Singh <vijaysg@altair.com>
2024-11-15 14:16:47 +0530

    Stage 3

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ def handle_connection(connection, address):
             arr_size, *arr = data.split(b"\r\n")
             res = [el.decode("utf-8") for el in arr[3::2]]
             mydict[res[0]] = res[1]
-            if res.size > 3:
+            if len(res) > 3:
                 threading.Timer(res[3] / 1000.0, lambda: mydict.pop(res[0], None)).start()
             print(f"mydict: {mydict}")
             response = b"+OK\r\n"
