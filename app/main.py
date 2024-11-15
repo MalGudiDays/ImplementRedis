d33eb4853f27980a837e69f18b227bc2a81e987b
Vijay Singh <vijaysg@altair.com>
2024-11-15 19:32:51 +0530

    Stage 3

--- app/main.py
+++ app/main.py
@@ -62,6 +62,10 @@ class Context:
             s = f"+FULLRESYNC "
             s += f"{self.replid.decode()} {self.repl_offset}\r\n"
             response = s.encode()
+            rdb_hex = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+            rdb_content = bytes.fromhex(rdb_hex)
+            rdb_data = f"${len(rdb_content)}\r\n".encode()
+            response += (rdb_data + rdb_content)
         return response
 
     def handle_connection(self, connection, address):
