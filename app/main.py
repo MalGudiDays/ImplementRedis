1a8bcbab2166aabc8a4e18b12917fb437ef14e97
Vijay Singh <vijaysg@altair.com>
2024-11-15 17:38:31 +0530

    Stage 3

--- app/main.py
+++ app/main.py
@@ -3,66 +3,69 @@ import socket
 import threading  # noqa: F401
 import time
 
-role = b"master"
 
-def redis_encode(data, encoding="utf-8"):
-    if not isinstance(data, list):
-        data = [data]
-    separator = "\r\n"
-    size = len(data)
-    encoded = []
-    for datum in data:
-        encoded.append(f"${len(datum)}")
-        encoded.append(datum)
-    if size > 1:
-        encoded.insert(0, f"*{size}")
-    print(f"encoded: {encoded}")
-    return (separator.join(encoded) + separator).encode(encoding=encoding)
+class Context:
+    def __init__(self, role=b"master"):
+        self.role = role
+        self.mydict = {}
 
-def handle_connection(connection, address):
-    mydict = {}
-    while True:
-        data = connection.recv(1024)
-        if not data:
-            break
-        response = b"-1\r\n"
-        print(data)
-        if b"ECHO" in data:
-            arr_size, *arr = data.split(b"\r\n")
-            response = redis_encode([el.decode("utf-8") for el in arr[3::2]])
-        elif b"PING" in data:
-            response = b"+PONG\r\n"
-        elif b"SET" in data:
-            arr_size, *arr = data.split(b"\r\n")
-            res = [el.decode("utf-8") for el in arr[3::2]]
-            mydict[res[0]] = res[1]
-            print(f"res: {res}")
-            if len(res) > 3:
-                threading.Timer(float(res[3]) / 1000.0, lambda: mydict.pop(res[0], None)).start()
-            print(f"mydict: {mydict}")
-            response = b"+OK\r\n"
-        elif b"GET" in data:
-            arr_size, *arr = data.split(b"\r\n")
-            key = arr[-2].decode()
-            print(f"arr: {arr}")
-            print(f"key: {key}")
-            print(f"mydict: {mydict}")
-            try:
-                response = redis_encode(mydict[key])
-            except KeyError:
-                response = b"$-1\r\n"
-        elif b"INFO" in data:
-             response = f"role: {role.decode()}"
-             response = redis_encode(response.encode())
+    def redis_encode(data, encoding="utf-8"):
+        if not isinstance(data, list):
+            data = [data]
+        separator = "\r\n"
+        size = len(data)
+        encoded = []
+        for datum in data:
+            encoded.append(f"${len(datum)}")
+            encoded.append(datum)
+        if size > 1:
+            encoded.insert(0, f"*{size}")
+        print(f"encoded: {encoded}")
+        return (separator.join(encoded) + separator).encode(encoding=encoding)
 
-        connection.send(response)
-
-def implement_redis_ping(port):
-    with socket.create_server(("localhost", port), reuse_port=False) as server_socket:
+    def handle_connection(connection, address):
+        mydict = {}
         while True:
-            connection, address = server_socket.accept()
-            client_thread = threading.Thread(target=handle_connection, args=(connection, address))
-            client_thread.start()
+            data = connection.recv(1024)
+            if not data:
+                break
+            response = b"-1\r\n"
+            print(data)
+            if b"ECHO" in data:
+                arr_size, *arr = data.split(b"\r\n")
+                response = redis_encode([el.decode("utf-8") for el in arr[3::2]])
+            elif b"PING" in data:
+                response = b"+PONG\r\n"
+            elif b"SET" in data:
+                arr_size, *arr = data.split(b"\r\n")
+                res = [el.decode("utf-8") for el in arr[3::2]]
+                mydict[res[0]] = res[1]
+                print(f"res: {res}")
+                if len(res) > 3:
+                    threading.Timer(float(res[3]) / 1000.0, lambda: mydict.pop(res[0], None)).start()
+                print(f"mydict: {mydict}")
+                response = b"+OK\r\n"
+            elif b"GET" in data:
+                arr_size, *arr = data.split(b"\r\n")
+                key = arr[-2].decode()
+                print(f"arr: {arr}")
+                print(f"key: {key}")
+                print(f"mydict: {mydict}")
+                try:
+                    response = redis_encode(mydict[key])
+                except KeyError:
+                    response = b"$-1\r\n"
+            elif b"INFO" in data:
+                response = redis_encode([f"role: {role.decode()}"])
+
+            connection.send(response)
+
+    def implement_redis_ping(port):
+        with socket.create_server(("localhost", port), reuse_port=False) as server_socket:
+            while True:
+                connection, address = server_socket.accept()
+                client_thread = threading.Thread(target=handle_connection, args=(connection, address))
+                client_thread.start()
             
 def main():
     parser = argparse.ArgumentParser(description="Redis-like server")
@@ -70,10 +73,11 @@ def main():
     parser.add_argument("--replicaof", type=str, help="Port number to listen on")
     args = parser.parse_args()
     print(f"args {args}")
+    x = Context(args.port)
     if args.replicaof:
-        global role
-        role = b"slave"
-    implement_redis_ping(args.port)
+        x.role = b"slave"
+        
+    x.implement_redis_ping(args.port)
 
 
 
