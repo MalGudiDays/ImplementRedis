f64b2f402ea3f451608bd897dc32118eadb62a89
Vijay Singh <vijaysg@altair.com>
2024-11-15 13:30:41 +0530

    Stage 3

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def handle_connection(connection, address):
             response = data.split("\r\n")[-2]
             ln = len(response)
             response = f"${ln}\r\n{response}\r\n"
-        connection.send(response.encode())
+        connection.send(response)
 
 def implement_redis_ping():
     with socket.create_server(("localhost", 6379), reuse_port=False) as server_socket:
