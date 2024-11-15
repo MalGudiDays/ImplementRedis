c177cdf0c4197137c33c2424c15474251cf9e79c
Vijay Singh <vijaysg@altair.com>
2024-11-15 11:14:53 +0530

    Stage 2

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import socket  # noqa: F401
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=False)
     connecton, _ = server_socket.accept()
-    connecton.sendall(b"+OK\r\n")
+    connecton.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
