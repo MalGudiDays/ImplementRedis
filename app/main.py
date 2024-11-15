f3df4b938cb5cde7d4e1b6aae812574668acf7f0
Vijay Singh <vijaysg@altair.com>
2024-11-15 13:29:02 +0530

    Stage 3

--- app/main.py
+++ app/main.py
@@ -1,16 +1,23 @@
 import socket
 import threading  # noqa: F401
 
-def handle_connection(connection):
-    while connection.recv(1024):
+def handle_connection(connection, address):
+    while True:
+        data = connection.recv(1024).decode("utf-8")
+        if not data:
+            break
         response = b"+PONG\r\n"
-        connection.send(response)
+        if "echo" in data:
+            response = data.split("\r\n")[-2]
+            ln = len(response)
+            response = f"${ln}\r\n{response}\r\n"
+        connection.send(response.encode("utf-8"))
 
 def implement_redis_ping(server_socket):
     with socket.create_server(("localhost", 6379), reuse_port=False) as server_socket:
         while True:
-            connection, _ = server_socket.accept()
-            client_thread = threading.Thread(target=handle_connection, args=(connection,))
+            connection, address = server_socket.accept()
+            client_thread = threading.Thread(target=handle_connection, args=(connection, address))
             client_thread.start()
 
 def main():
