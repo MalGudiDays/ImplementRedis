3aba09a00540f0039ca3ae80b4558643771162d4
Vijay Singh <vijaysg@altair.com>
2024-11-15 13:23:15 +0530

    Stage 3

--- app/main.py
+++ app/main.py
@@ -2,35 +2,16 @@ import socket
 import threading  # noqa: F401
 
 def handle_connection(connection):
-    while True:
-        data = connection.recv(1024)
-        print(data)
-        if not data:
-            break
-        print(data)
-        response = redis_encode([el.decode("utf-8") for el in data[3::2]])
+    while connection.recv(1024):
+        response = b"+PONG\r\n"
         connection.send(response)
 
-def implement_redis_ping():
+def implement_redis_ping(server_socket):
     with socket.create_server(("localhost", 6379), reuse_port=False) as server_socket:
         while True:
             connection, _ = server_socket.accept()
-            #client_thread = threading.Thread(target=handle_connection, args=(connection,))
-            #client_thread.start()
-            handle_connection(connection)
-
-def redis_encode(data, encoding="utf-8"):
-    if not isinstance(data, list):
-        data = [data]
-    separator = "\r\n"
-    size = len(data)
-    encoded = []
-    for datum in data:
-        encoded.append(f"${len(datum)}")
-        encoded.append(datum)
-    if size > 1:
-        encoded.insert(0, f"*{size}")
-    return (separator.join(encoded) + separator).encode(encoding=encoding)
+            client_thread = threading.Thread(target=handle_connection, args=(connection,))
+            client_thread.start()
 
 def main():
     implement_redis_ping()
